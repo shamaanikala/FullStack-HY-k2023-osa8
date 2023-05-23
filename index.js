e1d2c9083f9102127f276b6eb311ae5fc2404110
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-05-23 19:59:05 +0300

    T8.6 alkuun

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 const { ApolloServer } = require("@apollo/server")
 const { startStandaloneServer } = require("@apollo/server/standalone")
 
+const { v1: uuid } = require("uuid")
+
 let authors = [
   {
     name: "Robert Martin",
@@ -109,6 +111,7 @@ const typeDefs = `
 
   type Author {
     name: String!
+    id: ID!
     bookCount: Int!
   }
 
@@ -117,6 +120,15 @@ const typeDefs = `
     authorCount: Int!
     allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
+  }
+
+  type Mutation {
+    addBook(
+      title: String!
+      author: String!
+      published: Int!
+      genres: [String!]!
+    ): Book
   }
 `
 
@@ -137,6 +149,13 @@ const resolvers = {
   Author: {
     bookCount: ({ name }) => {
       return books.filter(b => b.author === name).length
+    },
+  },
+  Mutation: {
+    addBook: (root, args) => {
+      const book = { ...args, id: uuid() }
+      books = books.concat(book)
+      return book
     },
   },
 }
