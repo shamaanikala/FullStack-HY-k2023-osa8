5002214311ea6a3e664cff8945852cc851a2460d
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-05-23 18:53:02 +0300

    T8.2 valmis

--- index.js
+++ index.js
@@ -99,10 +99,18 @@ let books = [
 */
 
 const typeDefs = `
+  type Book {
+    title: String!
+    author: String!
+    published: Int!
+    genres: [String!]!
+    id: ID!
+  }
 
   type Query {
     bookCount: Int!
     authorCount: Int!
+    allBooks: [Book!]!
   }
 `
 
@@ -110,6 +118,7 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
+    allBooks: () => books,
   },
 }
 
