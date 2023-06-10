03c3293a1f7aa9051ae0b3bb47b708f655e2b181
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-06-10 16:07:55 +0300

    T8.13 lisätään MongoDB yhteys

--- library-backend/index.js
+++ library-backend/index.js
@@ -4,6 +4,26 @@ const { startStandaloneServer } = require('@apollo/server/standalone')
 
 const { v1: uuid } = require('uuid')
 
+const mongoose = require('mongoose')
+mongoose.set('strictQuery', false)
+
+const Author = require('./models/author')
+const Book = require('./models/book')
+
+require('dotenv').config()
+
+const MONGODB_URI = process.env.MONGODB_URI
+console.log('connecting to', MONGODB_URI)
+
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch(error => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
+
 let authors = [
   {
     name: 'Robert Martin',
