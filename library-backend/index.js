0236fd407fd0ab8d9753c46ff1834749b79b2bf1
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-06-11 12:45:22 +0300

    T8.16 lisätään login resolveri

--- library-backend/index.js
+++ library-backend/index.js
@@ -177,6 +177,25 @@ const resolvers = {
         })
       })
     },
+
+    login: async (root, args) => {
+      const user = await User.findOne({ username: args.username })
+
+      if (!user || args.password !== 'salainen') {
+        throw new GraphQLError('wrong credentials', {
+          extensions: {
+            code: 'BAD_USER_INPUT',
+          },
+        })
+      }
+
+      const userForToken = {
+        username: user.username,
+        id: user._id,
+      }
+
+      return { value: jwt.sign(userForToken, process.env.JWT_SECRET) }
+    },
   },
 }
 
