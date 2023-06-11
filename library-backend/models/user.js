75ad208e4a7590d24c11b47648dd7b95b5d0322f
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-06-11 12:18:22 +0300

    T8.16 lisätään mongoose schema User

---
+++ library-backend/models/user.js
@@ -0,0 +1,16 @@
+// Osa8c materiaalista mallia
+const mongoose = require('mongoose')
+
+const schema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    minlength: 3,
+  },
+  favoriteGenre: {
+    type: String,
+    required: true,
+  },
+})
+
+module.exports = mongoose.model('User', schema)
