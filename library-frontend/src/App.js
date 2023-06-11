8ea51abb483e6d063a8e7a92b88e273327dc3165
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-06-11 18:15:03 +0300

    T8.18 lisätään logout

--- library-frontend/src/App.js
+++ library-frontend/src/App.js
@@ -3,10 +3,18 @@ import Authors from './components/Authors'
 import Books from './components/Books'
 import NewBook from './components/NewBook'
 import LoginForm from './components/LoginForm'
+import { useApolloClient } from '@apollo/client'
 
 const App = () => {
   const [page, setPage] = useState('authors')
   const [token, setToken] = useState(null)
+  const client = useApolloClient()
+
+  const logout = () => {
+    setToken(null)
+    localStorage.clear()
+    client.resetStore()
+  }
 
   return (
     <div>
@@ -14,7 +22,8 @@ const App = () => {
         <button onClick={() => setPage('authors')}>authors</button>
         <button onClick={() => setPage('books')}>books</button>
         {token && <button onClick={() => setPage('add')}>add book</button>}
-        <button onClick={() => setPage('login')}>login</button>
+        {!token && <button onClick={() => setPage('login')}>login</button>}
+        {token && <button onClick={logout}>logout</button>}
       </div>
 
       <Authors show={page === 'authors'} />
