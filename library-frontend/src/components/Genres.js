0a23f27025fa6976752c7edd5aecf78ee80048c4
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-06-11 22:17:28 +0300

    T8.19 kaikki löydetyt genret listataan

--- library-frontend/src/components/Genres.js
+++ library-frontend/src/components/Genres.js
@@ -1,11 +1,52 @@
+import { useQuery } from '@apollo/client'
+import { ALL_GENRES } from '../queries'
+import { useEffect, useState } from 'react'
+
+const parseGenres = query => {
+  // query.data.allBooks sisältää taulukon olioita, joissa on
+  // kussakin taulukko genres. Ladataan nämä yksittäiset genret
+  // JavaScriptin joukkoon:
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Set
+
+  let genreSet = new Set()
+  console.log(query)
+  for (const bookObject of query) {
+    for (const genre of bookObject.genres) {
+      genreSet.add(genre)
+    }
+  }
+  console.log(genreSet)
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/from
+  const result = Array.from(genreSet)
+  return result
+}
+
 const Genres = props => {
+  const [genres, setGenres] = useState([])
+  const genresQuery = useQuery(ALL_GENRES)
+  console.log(genresQuery)
+
+  useEffect(() => {
+    genresQuery.loading
+      ? setGenres(['loading...'])
+      : setGenres(parseGenres(genresQuery.data.allBooks))
+  }, [setGenres, genresQuery.data.allBooks]) // eslint-disable-line
+
+  console.log(genres)
+
   if (!props.show) {
     return null
   }
 
   return (
     <div>
-      <b>genres here</b>
+      <h3>genret</h3>
+      <ul>
+        {genres.map(genre => (
+          <li key={genre}>{genre}</li>
+        ))}
+      </ul>
+      <h3>{genres.length}</h3>
     </div>
   )
 }
