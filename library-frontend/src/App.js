fba95373a444868f0e5fd4a414da55e257bcc1bd
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-06-13 17:30:29 +0300

    T8.24 valmis

--- library-frontend/src/App.js
+++ library-frontend/src/App.js
@@ -3,14 +3,23 @@ import Authors from './components/Authors'
 import Books from './components/Books'
 import NewBook from './components/NewBook'
 import LoginForm from './components/LoginForm'
-import { useApolloClient } from '@apollo/client'
+import { useApolloClient, useSubscription } from '@apollo/client'
 import Recommendations from './components/Recommendations'
+import { BOOK_ADDED } from './queries'
 
 const App = () => {
   const [page, setPage] = useState('authors')
   const [token, setToken] = useState(null)
   const client = useApolloClient()
 
+  useSubscription(BOOK_ADDED, {
+    onData: ({ data }) => {
+      // data.data.bookAdded.title
+      window.alert(`New book '${data.data.bookAdded.title}' added!`)
+      console.log(data)
+      // console.log(`kirja: ${data.data.bookAdded.title}`)
+    },
+  })
   const logout = () => {
     setToken(null)
     localStorage.clear()
